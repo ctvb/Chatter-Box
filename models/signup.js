3f311e087e86763351c0d8bbfee61beb790ca513
nwalk33 <nwalk33@yahoo.com>
2023-04-20 11:59:17 -0700

    signup/login js cloudinary

---
+++ models/signup.js
@@ -0,0 +1,44 @@
+let profile_image = " "
+const signupFormHandler = async (event) => {
+    event.preventDefault();
+  
+    const username = document.querySelector('#name-signup').value.trim();
+    const email = document.querySelector('#email-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+    const id = document.querySelector('#id-signup').value.trim();
+    const user_id = document.querySelector('#user_id-signup').value.trim();
+  
+    if (id && user_id && username && email && password) {
+      const response = await fetch('/api/users', {
+        method: 'POST',
+        body: JSON.stringify({ id, user_id, username, email, password, profile_image }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/chat');
+      } else {
+        alert(response.statusText);
+      }
+    }
+  };
+
+  var myWidget = cloudinary.createUploadWidget({
+    cloudName: 'dov0ohe0b', 
+    uploadPreset: 'eihxr5yn'}, (error, result) => { 
+      if (!error && result && result.event === "success") { 
+        console.log('Done! Here is the image info: ', result.info); 
+        profile_image = result.info.secure_url;
+      }
+    }
+  )
+  
+  document.getElementById("upload_widget").addEventListener("click", function(){
+      myWidget.open();
+    }, false);
+  
+  
+  document
+    .querySelector('.signup-form')
+    .addEventListener('submit', signupFormHandler);
+  
