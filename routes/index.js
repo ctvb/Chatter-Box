ee8d41e69d805ddb63a3a98fc298e658a1565a25
nwalk33 <nwalk33@yahoo.com>
2023-04-20 14:04:06 -0700

    added user routes

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,12 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
+const userRoutes = require('./userRoutes');
+const chatRoutes = require('./chat');
 
-// router.use('/api', apiRoutes);
+router.use('/api', apiRoutes);
+router.use('/userRoutes', userRoutes);
+router.use('/chat', chatRoutes);
+
 
 // router.use((req, res) => {
 //     res.send("<h1>Wrong Route!</h1>")
