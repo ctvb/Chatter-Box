04c0cca70b6c82bf1d284e9aeb7cc84af88d786d
Brittany Boughter <brittany.wood0308@yahoo.com>
2023-04-21 12:34:20 -0700

    added public static to server.js and changed route on main handlebars

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(express.static('public'));
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
