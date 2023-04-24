277ce651fd354928480a7cd4accd1e50c7087ed7
Brittany Boughter <brittany.wood0308@yahoo.com>
2023-04-24 14:34:47 -0700

    login page showing up

--- routes/api/userRoute.js
+++ routes/api/userRoute.js
@@ -3,38 +3,38 @@ const bcrypt = require('bcrypt');
 const { User } = require('../../models');
 const { request } = require('express');
 
-router.post('/', async (req, res) => {
-  try {
-    // console.log(req.body);
-    const hashedPassword = await bcrypt.hash(req.body.password, 10);
-    const userData = await User.create({
-      firstname: req.body.firstname,
-      username: req.body.username,
-      email: req.body.email,
-      password: hashedPassword
-       // Store the hashed password in the database
-    });
+// router.post('/', async (req, res) => {
+//   try {
+//     // console.log(req.body);
+//     const hashedPassword = await bcrypt.hash(req.body.password, 10);
+//     const userData = await User.create({
+//       firstname: req.body.firstname,
+//       username: req.body.username,
+//       email: req.body.email,
+//       password: hashedPassword
+//        // Store the hashed password in the database
+//     });
 
-    req.session.save(() => {
-      req.session.user_id = userData.id;
-      req.session.logged_in = true;
+//     req.session.save(() => {
+//       req.session.user_id = userData.id;
+//       req.session.logged_in = true;
 
-      res.status(200).json(userData);
-    });
-  } catch (err) {
-    console.log(err);
-    res.status(400).json(err);
-  }
-});
+//       res.status(200).json(userData);
+//     });
+//   } catch (err) {
+//     console.log(err);
+//     res.status(400).json(err);
+//   }
+// });
 
 router.post('/login', async (req, res) => {
   try {
-    const userData = await User.findOne({ where: { email: req.body.email } });
+    const userData = await User.findOne({ where: { username: req.body.username } });
 
     if (!userData) {
       res
         .status(400)
-        .json({ message: 'Incorrect email or password, please try again' });
+        .json({ message: 'Incorrect username or password, please try again' });
       return;
     }
 
@@ -43,7 +43,7 @@ router.post('/login', async (req, res) => {
     if (!validPassword) {
       res
         .status(400)
-        .json({ message: 'Incorrect email or password, please try again' });
+        .json({ message: 'Incorrect username or password, please try again' });
       return;
     }
 
