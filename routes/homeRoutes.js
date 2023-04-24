bccbce376d1e02afe4f1addfab29f6b68010e9c8
Brittany Boughter <brittany.wood0308@yahoo.com>
2023-04-24 00:16:47 -0700

    homepage is login page

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -2,30 +2,30 @@ const router = require('express').Router();
 const { User, Chat } = require('../models');
 const withAuth = require('../utils/auth');
 
-router.get('/', async (req, res) => {
-  try {
-    // Get all projects and JOIN with user data
-    const userData = await User.findAll({
-      include: [
-        {
-          model: User,
-          attributes: ['name'],
-        },
-      ],
-    });
+// router.get('/', async (req, res) => {
+//   try {
+//     // Get all projects and JOIN with user data
+//     const userData = await User.findAll({
+//       include: [
+//         {
+//           model: User,
+//           attributes: ['name'],
+//         },
+//       ],
+//     });
 
-    // Serialize data so the template can read it
-    const users = userData.map((user) => user.get({ plain: true }));
+//     // Serialize data so the template can read it
+//     const users = userData.map((user) => user.get({ plain: true }));
 
-    // Pass serialized data and session flag into template
-    res.render('homepage', { 
-      users, 
-      logged_in: req.session.logged_in 
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+//     // Pass serialized data and session flag into template
+//     res.render('homepage', { 
+//       users, 
+//       logged_in: req.session.logged_in 
+//     });
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
 
 // router.get('/user/:id', async (req, res) => {
 //   try {
@@ -69,14 +69,14 @@ router.get('/', async (req, res) => {
 //   }
 // });
 
-router.get('/login', (req, res) => {
+
+router.get('/', (req, res) => {
  
   if (req.session.logged_in) {
     res.redirect('/chat');
-    return;
+  } else {
+    res.render('homepage');
   }
-
-  res.render('login');
 });
 
 router.get('/signup', (req, res) => {
