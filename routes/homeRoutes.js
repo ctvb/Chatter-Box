862a423e9dbb6b4b42b6c51646cea655d52749cb
Brittany Boughter <brittany.wood0308@yahoo.com>
2023-04-24 00:34:43 -0700

    everything working so far but unable to test without logout function

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -1,74 +1,4 @@
 const router = require('express').Router();
-const { User, Chat } = require('../models');
-const withAuth = require('../utils/auth');
-
-// router.get('/', async (req, res) => {
-//   try {
-//     // Get all projects and JOIN with user data
-//     const userData = await User.findAll({
-//       include: [
-//         {
-//           model: User,
-//           attributes: ['name'],
-//         },
-//       ],
-//     });
-
-//     // Serialize data so the template can read it
-//     const users = userData.map((user) => user.get({ plain: true }));
-
-//     // Pass serialized data and session flag into template
-//     res.render('homepage', { 
-//       users, 
-//       logged_in: req.session.logged_in 
-//     });
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
-
-// router.get('/user/:id', async (req, res) => {
-//   try {
-//     const userData = await User.findByPk(req.params.id, {
-//       include: [
-//         {
-//           model: User,
-//           attributes: ['name'],
-//         },
-//       ],
-//     });
-
-//     const users = userData.get({ plain: true });
-
-//     res.render('user', {
-//       ...project,
-//       logged_in: req.session.logged_in
-//     });
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
-
-// // Use withAuth middleware to prevent access to route
-// router.get('/profile', withAuth, async (req, res) => {
-//   try {
-//     // Find the logged in user based on the session ID
-//     const userData = await User.findByPk(req.session.user_id, {
-//       attributes: { exclude: ['password'] },
-//       include: [{ model: Project }],
-//     });
-
-//     const user = userData.get({ plain: true });
-
-//     res.render('profile', {
-//       ...user,
-//       logged_in: true
-//     });
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
-
 
 router.get('/', (req, res) => {
  
@@ -77,6 +7,11 @@ router.get('/', (req, res) => {
   } else {
     res.render('homepage');
   }
+});
+
+router.get('/login', (req, res) => {
+  res.render('login');
+  return;
 });
 
 router.get('/signup', (req, res) => {
